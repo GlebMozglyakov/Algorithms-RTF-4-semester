ac28adbe9774723878857f41ebb22384daa372d2
GlebMozglyakov <mozglgleb2004@gmail.com>
2024-03-24 11:33:47 +0500

    Update

--- 4. Stack and deque/Task 3.py
+++ 4. Stack and deque/Task 3.py
@@ -1,32 +1,53 @@
-def process_queries(queries):
-    queue = []
-    knights = []
+class SpecialQueue:
+    def __init__(self):
+        self.queue = []
 
-    for query in queries:
-        command = query.split()
+    def enqueue(self, knight_id):
+        self.queue.append(knight_id)
 
+    def enqueue_middle(self, knight_id):
+        middle_position = len(self.queue) // 2 + len(self.queue) % 2
+
+        self.queue.insert(middle_position, knight_id)
+
+    def enqueue_front(self, knight_id):
+        self.queue.insert(0, knight_id)
+
+    def dequeue(self):
+        if self.queue:
+            return self.queue.pop(0)
+
+        return None
+
+
+def process_commands(commands):
+    queue = SpecialQueue()
+    results = []
+
+    for command in commands:
+        command = command.split()
         match command[0]:
             case "+":
-                queue.append(int(command[1]))
+                queue.enqueue(int(command[1]))
             case "*":
-                middle_position = len(queue) // 2 + len(queue) % 2
-                queue.insert(middle_position, int(command[1]))
+                queue.enqueue_middle(int(command[1]))
             case "!":
-                queue.insert(0, int(command[1]))
+                queue.enqueue_front(int(command[1]))
             case "-":
-                knights.append(queue.pop(0))
-
-    return knights
+                knight = queue.dequeue()
+                if knight is not None:
+                    results.append(knight)
+    return results
 
 
 def main():
     n = int(input())
-    queries = [input() for _ in range(n)]
+    commands = [input() for _ in range(n)]
 
-    knights = process_queries(queries)
+    results = process_commands(commands)
 
-    for knight in knights:
-        print(knight)
+    for result in results:
+        print(result)
 
 
 if __name__ == "__main__":
