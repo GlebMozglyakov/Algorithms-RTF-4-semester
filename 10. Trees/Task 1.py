c951087e182f0cb1a78f2e8d388f3a4fdd899d98
GlebMozglyakov <mozglgleb2004@gmail.com>
2024-05-05 16:12:45 +0500

    Update

---
+++ 10. Trees/Task 1.py
@@ -0,0 +1,32 @@
+class Node:
+    def __init__(self, index):
+        self.index = index
+        self.nodes = []
+
+
+def get_all_needed_nodes(node, visited_nodes):
+    for cur_node in node.nodes:
+        if cur_node not in visited_nodes:
+            visited_nodes.add(cur_node)
+            get_all_needed_nodes(cur_node, visited_nodes)
+
+
+n, v = map(int, input().split())
+adjacent_nodes = [Node(i) for i in range(n)]
+
+for node in adjacent_nodes:
+    for i in map(int, input().split()):
+        if i == -1:
+            continue
+        cur = adjacent_nodes[i]
+        node.nodes.append(cur)
+
+needed_node = adjacent_nodes[v]
+
+visited_nodes = {needed_node}
+get_all_needed_nodes(needed_node, visited_nodes)
+
+result = sorted(visited_nodes, key=lambda e: e.index)
+result = [x.index for x in result]
+
+print(*result)
